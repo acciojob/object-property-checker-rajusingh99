a5d91ab276284ef45891f18c0d31c1c444176c43
Raju Singh <94919614+rajusingh99@users.noreply.github.com>
2023-07-08 13:40:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  //   write your code here
+  return sampleObject.hasOwnProperty(key);
 }
 
-// Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
+// Testing the function
+console.log(hasKey("red"));    // true
+console.log(hasKey("blue"));   // false
+console.log(hasKey("white"));  // true
